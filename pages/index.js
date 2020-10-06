c3334486904cede6c41f17a6b7d2560c8d5ed3da
IamNiters <ismael.bernard69@gmail.com>
2020-10-06 10:44:20 +0200

    issou

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
+          <img src="/isma.png" alt="Ismael se repose" />
         </h1>
 
         <p className={styles.description}>
