b3bc7f20c97983ae358dd4f9e5f4c95f45dffe53
Myst3k69 <aurelien.chiren@gmail.com>
2020-10-09 17:20:12 +0200

    Adding pages User / Tags and Posts with sample data

---
+++ back/src/views/pages/tags/Tags.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import {
+  CBadge,
+  CCard,
+  CCardBody,
+  CCardHeader,
+  CCol,
+  CDataTable,
+  CRow
+} from '@coreui/react'
+
+import usersData from '../../users/UsersData'
+
+const getBadge = status => {
+  switch (status) {
+    case 'Active': return 'success'
+    case 'Inactive': return 'secondary'
+    case 'Pending': return 'warning'
+    case 'Banned': return 'danger'
+    default: return 'primary'
+  }
+}
+const fields = ['id','nom', 'created_at', 'created_by']
+
+
+const Tables = () => {
+  return (
+    <>
+
+
+      <CRow>
+        <CCol>
+          <CCard>
+            <CCardHeader>
+              Gestion des tags
+            </CCardHeader>
+            <CCardBody>
+              <CDataTable
+                items={usersData}
+                fields={fields}
+                hover
+                striped
+                bordered
+                size="sm"
+                itemsPerPage={10}
+                pagination
+                scopedSlots = {{
+                  'status':
+                    (item)=>(
+                      <td>
+                        <CBadge color={getBadge(item.status)}>
+                          {item.status}
+                        </CBadge>
+                      </td>
+                    )
+                }}
+              />
+            </CCardBody>
+          </CCard>
+        </CCol>
+      </CRow>
+
+    </>
+  )
+}
+
+export default Tables
---
+++ back/src/views/pages/users/Userlist.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import {
+  CBadge,
+  CCard,
+  CCardBody,
+  CCardHeader,
+  CCol,
+  CDataTable,
+  CRow
+} from '@coreui/react'
+
+import postsData from '../../users/PostsData'
+
+const getBadge = status => {
+  switch (status) {
+    case 'Active': return 'success'
+    case 'Inactive': return 'secondary'
+    case 'Pending': return 'warning'
+    case 'Banned': return 'danger'
+    default: return 'primary'
+  }
+}
+const fields = ['select','id','question','reponse', 'user']
+
+
+const Tables = () => {
+  return (
+    <>
+
+
+      <CRow>
+        <CCol>
+          <CCard>
+            <CCardHeader>
+              Gestion des utilisateurs
+            </CCardHeader>
+            <CCardBody>
+              <CDataTable
+                items={postsData}
+                fields={fields}
+                hover
+                striped
+                bordered
+                size="sm"
+                itemsPerPage={10}
+                pagination
+                scopedSlots = {{
+                  'status':
+                    (item)=>(
+                      <td>
+                        <CBadge color={getBadge(item.status)}>
+                          {item.status}
+                        </CBadge>
+                      </td>
+                    )
+                }}
+              />
+            </CCardBody>
+          </CCard>
+        </CCol>
+      </CRow>
+
+    </>
+  )
+}
+
+export default Tables
