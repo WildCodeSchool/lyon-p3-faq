b8054ebd0a1ea2675a9af6649c7403dc13bf72bc
Trad <trad.aidoud@gmail.com>
2020-11-06 09:34:41 +0100

    API with controler and model

--- api/app.js
+++ api/app.js
@@ -1,17 +1,17 @@
-const cookieParser = require('cookie-parser');
-const express = require('express');
-const httpErrors = require('http-errors');
-const logger = require('morgan');
-const path = require('path');
-let cors = require('cors');
-require('dotenv/config');
-let bodyParser = require('body-parser');
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const httpErrors = require("http-errors");
+const logger = require("morgan");
+const path = require("path");
+let cors = require("cors");
+require("dotenv/config");
+let bodyParser = require("body-parser");
 
-const indexRouter = require('./routes/index');
-const frontRouter = require('./routes/front/index');
-const userRouter = require('./routes/backoffice/user');
-const postRouter = require('./routes/backoffice/post');
-const loginRouter = require('./routes/backoffice/login');
+const indexRouter = require("./routes/index");
+const frontRouter = require("./routes/front/index");
+const userRouter = require("./routes/backoffice/user");
+const postRouter = require("./routes/backoffice/post");
+const loginRouter = require("./routes/backoffice/login");
 
 const app = express();
 
@@ -22,21 +22,22 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Router middleware
-app.use('/', indexRouter);
-app.use('/back/users', userRouter);
-app.use('/back/posts', postRouter);
-app.use('/back/login', loginRouter);
+app.use("/", indexRouter);
+app.use("/front", frontRouter);
+app.use("/back/users", userRouter);
+app.use("/back/posts", postRouter);
+app.use("/back/login", loginRouter);
 
-app.set('views', path.join(__dirname, 'views'));
+app.set("views", path.join(__dirname, "views"));
 
 // view engine setup
-app.set('view engine', 'ejs');
-app.use(logger('dev'));
+app.set("view engine", "ejs");
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', indexRouter);
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -47,11 +48,11 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- api/routes/front/index.js
+++ api/routes/front/index.js
@@ -1,64 +1,54 @@
 const express = require("express");
 const router = express.Router();
-const db = require('../../datasource/mysql');
+const db = require("../../datasource/mysql");
+const QuestionController = require("./controller");
+// router.get("/questions", function (req, res) {
+//   db.query(
+//     "SELECT question.titre, question.contenu FROM question JOIN reponse ON question.id = reponse.question_id",
+//     (err, results) => {
+//       if (err) {
+//         console.log(err);
+//         res.status(500).send("Erreur");
+//       } else {
+//         res.send(results);
+//       }
+//     }
+//   );
+// });
 
-router.get("/", function (req, res) {
-  res.send("Hello");
-});
+// router.get("/questionsAnswered", function (req, res) {
+//   db.query(
+//     "SELECT question.titre, question.contenu, reponse.contenu AS reponse FROM question JOIN reponse ON question.id = reponse.question_id",
+//     (err, results) => {
+//       if (err) {
+//         console.log(err);
+//         res.status(500).send("Erreur");
+//       } else {
+//         res.json(results);
+//       }
+//     }
+//   );
+// });
 
-router.get("/test", function (req, res) {
-  db.query("SELECT name FROM user", (err, results) => {
-    if (err) {
-      console.log(err);
-      res.status(500).send("Erreur");
-    } else {
-      res.send(results);
-    }
-  });
-});
+// router.post("/ask", function (req,res) {
+//   const title=`${req.body.titre}`;
+//   const contenu=`${req.body.contenu}`;
+//   db.query("INSERT INTO question (titre, contenu) VALUES (?)",[[title, contenu]], (err, results) => {
+//     if (err) {
+//       console.log(err);
+//       res.status(500).send({message: "Erreur"});
+//     } else if(req.body.titre.length<100 && req.body.contenu.length<300 ){
+//       res.status(200).send({message: "post confirmed"});
+//     }
+//     else{
+//       res.status(412).send({message: "Mauvais format"});
+//     }
+//   })
+// });
 
-router.get("/questionAnswered", function (req, res) {
-  db.query(
-    "SELECT question.titre, question.contenu FROM question JOIN reponse ON question.id = reponse.question_id",
-    (err, results) => {
-      if (err) {
-        console.log(err);
-        res.status(500).send("Erreur");
-      } else {
-        res.send(results);
-      }
-    }
-  );
-});
-
-router.get("/question-answer", function (req, res) {
-  db.query(
-    "SELECT question.titre, question.contenu, reponse.contenu AS reponse FROM question JOIN reponse ON question.id = reponse.question_id",
-    (err, results) => {
-      if (err) {
-        console.log(err);
-        res.status(500).send("Erreur");
-      } else {
-        res.json(results);
-      }
-    }
-  );
-});
-
-router.post("/ask", function (req,res) {
-  const title="${req.body.titre}";
-  const contenu="${req.body.contenu}";
-  db.query("INSERT INTO question (titre, contenu) VALUES (?)",[[title, contenu]], (err, results) => {
-    if (err) {
-      console.log(err);
-      res.status(500).send({message: "Erreur"});
-    } else if(req.body.titre.length<100 && req.body.contenu.length<300 ){
-      res.status(200).send({message: "post confirmed"});
-    }
-    else{
-      res.status(412).send({message: "Mauvais format"});
-    }
-  })
-});
+// module.exports = router;
+router.get("/", QuestionController.getAll);
+router.get("/answered", QuestionController.getAllAnswered);
+router.post("/", QuestionController.postQuestion);
 
 module.exports = router;
