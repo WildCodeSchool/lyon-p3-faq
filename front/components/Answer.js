5dc105d12047d84816961b55cb8f37de1f1b3cc3
IsmaelBernard <ismael.bernard69@gmail.com>
2020-11-12 16:27:53 +0100

    Added WIP Answer components

---
+++ front/components/Answer.js
@@ -0,0 +1,39 @@
+import react from "react";
+import styles from "../styles/Answer.module.css";
+
+const Answer = () => (
+<body className={styles.body}>
+  <div className={styles.wrapper}>
+    <div className={styles.box}>
+      <div className={styles.question}>
+        <h2>Taux horaire débutant</h2>
+        <p>
+          Salut à tous, je débute mon activité de frelance et la question que je
+          me pose est : comment calculer mon taux horaire ( ou journalier ).
+          Quelles sont pour vous les éléments ( techno, frais etc... ) qui font
+          fluctuer ce taux?
+        </p>
+        <span>
+          <p>Mathias</p>
+        </span>
+      </div>
+      <div className={styles.answer}>
+        <p>
+          Salut Mathias, il n’existe pas de réponse universelle. Regarde les
+          tarifs pratiqués dans ta région, ne pratique pas de tarifs trop bas en
+          espérant attirer plus de clients. L'astuce Vegan pas banal de
+          GoodPlanet : facile la mousse au chocolat vegan en remplaçant les
+          oeufs par d'eau des pois chiches. Monter en neige de l'eau de cuisson
+          de pois chiches à la place des œufs: 3 cuillères à soupe d'aquafaba =
+          1 œuf.
+        </p>
+        <span>
+          <p>Caroline</p>
+        </span>
+      </div>
+    </div>
+  </div>
+</body>
+);
+
+export default Answer;
--- front/pages/index.js
+++ front/pages/index.js
@@ -1,9 +1,9 @@
 import Head from 'next/head'
 import Header from '../components/Header'
-import Main from '../components/Main'
 import Footer from '../components/Footer'
 
 import styles from '../styles/Home.module.css'
+import Answer from '../components/Answer'
 
 export default function Home() {
   return (
@@ -13,7 +13,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header/>
-      <Main/> 
+      <Answer/>
     </div>
   )
 }
