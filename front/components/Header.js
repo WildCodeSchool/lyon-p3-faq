3120623c41ca26bfa74c5f75ee99f5170d4507cf
IsmaelBernard <ismael.bernard69@gmail.com>
2020-11-17 21:06:17 +0100

    Added wrapper in Header for UHD devices

--- front/components/Header.js
+++ front/components/Header.js
@@ -3,8 +3,10 @@ import styles from "../styles/Header.module.css";
 
 const Header = () => (
     <header className={styles.header}>
+        <span className={styles.headerWrapper}>
         <Link href="/"><h1> Les Freelances Lyonnais</h1></Link>
         <button>Poser une question</button>
+        </span>
     </header>
 )
 
