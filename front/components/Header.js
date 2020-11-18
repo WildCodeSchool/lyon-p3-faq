34a5d09637efd062d11e213bb9d67169775fa610
IsmaelBernard <ismael.bernard69@gmail.com>
2020-11-18 09:39:24 +0100

    Added Hamburger Menu Icon

--- front/components/Header.js
+++ front/components/Header.js
@@ -11,6 +11,14 @@ export default function Header() {
         <Link href="/"><h1> Les Freelances Lyonnais</h1></Link>
         <button onClick={toggle}>Poser une question</button>
         <Modal isShowing={isShowing} hide={toggle} />
+        <span>
+        <img className={styles.menuHam}
+              src="/sqaure_ham_menu_white.png"
+              height="25px"
+              width="25x"
+              alt="Hamburger_menu_white"
+            />
+        </span>
         </span>
     </header>
   );
