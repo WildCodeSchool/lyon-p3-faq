e101730e23b342c2136c2fa884e2d970af031de3
Myst3k69 <aurelien.chiren@gmail.com>
2020-11-18 09:22:14 +0000

    deleting console log

--- api/datasource/mysql.js
+++ api/datasource/mysql.js
@@ -2,9 +2,20 @@ const mysql = require("mysql");
 const dotenv = require("dotenv").config();
 
 class DB {
-  static query(query, ...params) {
+  init() {
+    this.connection = mysql.createPool({
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      host: process.env.MYSQL_HOST,
+      database: process.env.MYSQL_DATABASE,
+    });
+    return this;
+  }
+
+  async query(query, ...params) {
+    
     return new Promise((resolve, reject) => {
-      connection.query(query, params, (err, result) => {
+      this.connection.query(query, params, (err, result) => {
         if (err) {
           reject(err);
         } else {
@@ -13,13 +24,15 @@ class DB {
       });
     });
   }
+
+  async queryt(...args) {
+    return new Promise((resolve, reject) => {
+      this.connection.query(...args, (err, res) => {
+        if (err) reject(err);
+        else resolve(res);
+      });
+    });
+  }
 }
 
-let connection = mysql.createPool({
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  host: process.env.MYSQL_HOST,
-  database: process.env.MYSQL_DATABASE,
-});
-
-module.exports = { DB, connection };
+module.exports = new DB().init();
