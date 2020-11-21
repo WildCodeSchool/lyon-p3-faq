c2c0728cb725316e16f194e334faf83a212e7c7a
AIDOUD Trad <trad.aidoud@gmail.com>
2020-11-21 21:33:05 +0100

    link API front. Need to add user's question in API and add in the comp Main

--- front/components/Main.js
+++ front/components/Main.js
@@ -1,149 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../styles/Main.module.css";
-import Link from 'next/link';
+import Link from "next/link";
 
-export default function Main() {
+export default function Main(props) {
+  const [test, setTest] = useState({});
+
+  useEffect(() => {
+    setTest(props.questions);
+  }, []);
+
   return (
-    <section className={styles.section}>
+    <>
+        <section className={styles.section}>
       <main className={styles.main}>
       <h2 className={styles.texth2}>Questions récentes</h2>
         <div className={styles.wrapper}>
+      {Object.entries(test).map(([key]) => {
+        return (
           <div className={styles.question}>
-            <p className={styles.p}>
-              Lorstrong ipsum dolor sit amet, consectetur adipiscing elit.
-              Mauris a fringilla odio, sed venenatis sapien. Aliquam vehicula
-              accumsan odio, a tstrongpor justo egestas sit amet. Nullam sit
-              amet mauris tincidunt, pharetra dolor quis, euismod mauris.
-              Phasellus venenatis nibh lacus, ut euismod dui aliquam vitae. Duis
-              tstrongpor quam at consequat lacinia ?
-            </p>
+            <h3>{test[key]["titre"]}</h3>
+            <p className={styles.p}>{test[key]["contenu"]}</p>
             <div className={styles.wrapperAuthor}>
-            <span>
-              <p>
-                <strong>Julien Bord</strong> à répondu
-              </p>
+              <span>
+                <p>
+                  <strong>Julien Bord</strong> à répondu
+                </p>
               </span>
 
               <span>
-              <Link as="answer" href="/question">
-              <a className={styles.RMbutton}>
-                Lire la réponse
-			        </a>
-				      </Link>
-            </span>
-            </div>
-          </div>
-
-          <div className={styles.question}>
-            <div>
-            <p className={styles.p}>
-              LDuis at tristique lorem, consequat lacinia arcu. Phasellus mollis
-              orci ac eleifend gravida. Donec vestibulum nisi congue, egestas
-              massa non, molestie nulla. Integer ac sodales justo. In euismod
-              consectetur tellus vel congue. Sed nisl nunc, fringilla nec est a,
-              rhoncus congue quam. Maecenas porta quam ligula, non tempus est
-              accumsan at. Quisque malesuada lorem et magna commodo, vel dictum
-              diam faucibus. [...]
-            </p>
-
-            </div>
-            <div className={styles.wrapperAuthor}>
-            <span>
-              <p>
-                <strong>Faber Castell</strong> à répondu
-              </p>
+                <Link as="answer" href="/question">
+                  <a className={styles.RMbutton}>Lire la réponse</a>
+                </Link>
               </span>
-
-              <span>
-              <Link as="answer" href="/question">
-              <a className={styles.RMbutton}>
-                Lire la réponse
-			        </a>
-				      </Link>
-            </span>
             </div>
           </div>
-
-          <div className={styles.question}>
-            <p className={styles.p}>
-              Lorstrong ipsum dolor sit amet, consectetur adipiscing elit.
-              Mauris a fringilla odio, sed venenatis sapien. Aliquam vehicula
-              accumsan odio, a tstrongpor justo egestas sit amet. Nullam sit
-              amet mauris tincidunt, pharetra dolor quis, euismod mauris.
-              Phasellus venenatis nibh lacus, ut euismod dui aliquam vitae. Duis
-              tstrongpor quam at consequat lacinia ? 
-            </p>
-            <div className={styles.wrapperAuthor}>
-            <span>
-              <p>
-                <strong>Antoine Vicard</strong> à répondu
-              </p>
-              </span>
-
-              <span>
-              <Link as="answer" href="/question">
-              <a className={styles.RMbutton}>
-                Lire la réponse
-			        </a>
-				      </Link>
-            </span>
-            </div>
-          </div>
-
-          <div className={styles.question}>
-            <p className={styles.p}>
-              Lorstrong ipsum dolor sit amet, consectetur adipiscing elit.
-              Mauris a fringilla odio, sed venenatis sapien. Aliquam vehicula
-              accumsan odio, a tstrongpor justo egestas sit amet. Nullam sit
-              amet mauris tincidunt, pharetra dolor quis, euismod mauris.
-              Phasellus venenatis nibh lacus, ut euismod dui aliquam vitae. Duis
-              tstrongpor quam at consequat lacinia ? 
-            </p>
-            <div className={styles.wrapperAuthor}>
-            <span>
-              <p>
-                <strong>Antoine Vicard</strong> à répondu
-              </p>
-              </span>
-
-              <span>
-              <Link as="answer" href="/question">
-              <a className={styles.RMbutton}>
-                Lire la réponse
-			        </a>
-				      </Link>
-            </span>
-            </div>
-          </div>
-
-          <div className={styles.question}>
-            <p className={styles.p}>
-			        LDuis at tristique lorem, consequat lacinia arcu. Phasellus mollis
-              orci ac eleifend gravida. Donec vestibulum nisi congue, egestas
-              massa non, molestie nulla. Integer ac sodales justo. In euismod
-              consectetur tellus vel congue. Sed nisl nunc, fringilla nec est a,
-              rhoncus congue quam. Maecenas porta quam ligula, non tempus est
-              accumsan at. Quisque malesuada lorem et magna commodo, vel dictum
-              diam faucibus. [...]
-            </p>
-            <div className={styles.wrapperAuthor}>
-            <span>
-              <p>
-                <strong>Amélie Buchanan</strong> à répondu
-              </p>
-              </span>
-
-              <span>
-              <Link as="answer" href="/question">
-              <a className={styles.RMbutton}>
-                Lire la réponse
-			        </a>
-				      </Link>
-            </span>
-            </div>
-          </div>
-        </div>
+        );
+      })}
+      </div>
       </main>
-    </section>
+      </section>
+    </>
   );
 }
--- front/pages/index.js
+++ front/pages/index.js
@@ -1,22 +1,32 @@
-import Head from 'next/head'
-import Header from '../components/Header'
-import Main from '../components/Main'
-import Footer from '../components/Footer';
+import React, { useEffect, useState } from "react";
+import Head from "next/head";
+import Header from "../components/Header";
+import Main from "../components/Main";
+import Footer from "../components/Footer";
 
+export default function Home({ data }) {
+  // console.log(data);
 
-
-export default function Home() {
   return (
     <>
       <Head>
         <title>La FAQ</title>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
       </Head>
-      <Header/>
-      <Main/>
-      <Footer/>
-      
+      <Header />
+      <Main questions={data}/>
+      <Footer />
     </>
-  )
+  );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(`http://localhost:3000/front`);
+  const data = await res.json();
+
+  return { props: { data } };
 }
