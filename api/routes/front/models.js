f2ac56c31c2adf544e04a3b05401d5611ecbac2b
AIDOUD Trad <trad.aidoud@gmail.com>
2020-11-22 01:56:51 +0100

    add cors and other info to send to the front

--- api/routes/front/models.js
+++ api/routes/front/models.js
@@ -3,7 +3,7 @@ const db = require("../../datasource/mysql");
 class Question {
   static async getQuestions() {
     return db.query(
-      "SELECT question.titre, question.contenu FROM question JOIN reponse ON question.id = reponse.question_id"
+      "SELECT question.titre, question.contenu, reponse.created_by FROM question JOIN reponse ON question.id = reponse.question_id"
     );
   }
 
