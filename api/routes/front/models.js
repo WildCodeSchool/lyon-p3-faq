60ac57c7320a403f680fc60f62c5ee51aa3d69d6
AIDOUD Trad <trad.aidoud@gmail.com>
2020-11-22 18:36:24 +0100

    add info

--- api/routes/front/models.js
+++ api/routes/front/models.js
@@ -3,21 +3,28 @@ const db = require("../../datasource/mysql");
 class Question {
   static async getQuestions() {
     return db.query(
-      "SELECT question.titre, question.contenu, reponse.created_by FROM question JOIN reponse ON question.id = reponse.question_id"
+      "SELECT question.titre, question.contenu, question.id, reponse.created_by FROM question JOIN reponse ON question.id = reponse.question_id WHERE question.disabled_at IS NULL"
     );
   }
 
-  static async getQuestionsAnswered() {
-    return db.query(
-      "SELECT question.titre, question.contenu, reponse.contenu AS reponse FROM question JOIN reponse ON question.id = reponse.question_id"
-    );
+  static async getQuestionsAnswered(id) {
+    return db
+      .query(
+        "SELECT question.titre, question.contenu, reponse.contenu AS reponse, reponse.created_by AS replyer, question.created_by AS asker FROM question JOIN reponse ON question.id = reponse.question_id WHERE question.id = ? AND reponse.disabled_at IS NULL",
+        [parseInt(id)]
+      )
+      .then((res) => {
+        return { res };
+      });
   }
 
   static async postQuestion(titre, contenu) {
     return db
-      .query("INSERT INTO question (titre, contenu) VALUES (?)", [[titre, contenu]])
+      .query("INSERT INTO question (titre, contenu) VALUES (?)", [
+        [titre, contenu],
+      ])
       .then((res) => {
-        return {res};
+        return { res };
       });
   }
 }
