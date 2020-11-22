fdbe9b8de8f5651632f78fd5e43de53cf6cc66bd
IsmaelBernard <ismael.bernard69@gmail.com>
2020-11-22 16:49:12 +0100

    added sidebar

--- front/components/Header.js
+++ front/components/Header.js
@@ -2,9 +2,17 @@ import Link from "next/link";
 import styles from "../styles/Header.module.css";
 import useModal from "../lib/useModal";
 import Modal from "./ButtonAsk";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import { useState } from "react";
+import {SidebarData} from "./SidebarData";
 
 export default function Header() {
     const { isShowing, toggle } = useModal();
+    const [sidebar, setSidebar] = useState(false);
+
+    const showSidebar = () => setSidebar(!sidebar);
+
   return (
     <header className={styles.header}>
         <span className={styles.headerWrapper}>
@@ -12,6 +20,30 @@ export default function Header() {
         <button onClick={toggle}>Poser une question</button>
         <Modal isShowing={isShowing} hide={toggle} />
         </span>
+        <span className={styles.navbar}>
+          <Link as="Home" href="/" className={styles.menu_bar}>
+            <FaIcons.FaBars onClick={showSidebar}/>
+          </Link>
+        </span>
+        <nav className={sidebar ? `{styles.nav-menu active}` : `{styles.nav-menu}`}>
+            <ul className={styles.nav_menu_items}>
+              <li className={styles.navbar-toggle}>
+                  <Link as="Home" href="/"  className={styles.menu_bar}>
+                    <AiIcons.AiOutlineCloseCircle />
+                  </Link>
+              </li>
+              {SidebarData.map((item, index) => {
+                return (
+                  <li key={index} className={item.cName}>
+                    <Link as={item.title} href={item.path}>
+                      {item.icon}
+                      <span>{item.title}</span>
+                    </Link>
+                  </li>
+                )
+              })}
+            </ul>
+        </nav>
     </header>
   );
 }
---
+++ front/components/SidebarData.js
@@ -0,0 +1,26 @@
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
+
+export const SidebarData = [
+    {
+        title : "Home",
+        path : "/",
+        icon : <AiIcons.AiFillHome />,
+        cName : "nav-text"
+    },
+
+    {
+        title : "About-us",
+        path : "/about-us",
+        icon : <FaIcons.FaInfo />,
+        cName : "nav-text"
+    },
+
+    {
+        title : "Mentions-legales",
+        path : "/mentions",
+        icon : <FaIcons.FaBalanceScale />,
+        cName : "nav-text"
+    },
+]
