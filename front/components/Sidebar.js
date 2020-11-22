86ca671d637c58154f08b238551672256960666f
IsmaelBernard <ismael.bernard69@gmail.com>
2020-11-22 11:48:26 +0100

    Added close icon button

--- front/components/Sidebar.js
+++ front/components/Sidebar.js
@@ -1,10 +1,28 @@
 
 function Sidebar () {
     return (
-        <div>
+        <>
+            <div className="navbar">
+                <Link to='#' className="menu-bar">
+                    <img 
+                        src="/menu.png"
+                        className="menu-icons"
+                        alt="menu icon"
+                        />
+                </Link>
 
-        </div>
-    )
+            </div>
+            <nav className= { sidebar ? "nav-menu active": "nav-menu" }>
+                <ul className="nav-menu-items">
+                    <li className="navbar-toggle">
+                        <Link to ="#" className="menu-bar">
+                            
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </>
+    );  
 }
 
 export default Sidebar;
