70a140da322167ef0b5543654706dfb933d3d335
Myst3k69 <aurelien.chiren@gmail.com>
2020-11-23 01:30:47 +0000

    finishing MVC Model

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,4 +1,3 @@
-
 const UserModel = require("../models/user.model");
 
 class UserController {
@@ -8,7 +7,7 @@ class UserController {
       let idUser = req.params.id;
 
       // On vérifie si l'utilisateur existe en base de données
-      const countResult = await UserModel.matchDB(idUser);
+      const countResult = await UserModel.matchUser(idUser);
 
       // L'utilisateur a bien été trouvé dans la base de données
 
@@ -21,9 +20,7 @@ class UserController {
           role_id: role,
         };
 
-
-        const queryResult = await UserModel.updateOne(idUser,fields);
-
+        const queryResult = await UserModel.update(idUser, fields);
 
         res.send("User successfully updated");
       } else {
@@ -38,17 +35,16 @@ class UserController {
 
   // get One user
   static async getOne(req, res) {
-    console.log("getOne");
     try {
       let idUser = req.params.id;
 
       // On vérifie si l'utilisateur existe en base de données
-      const countResult = await UserModel.matchDB(idUser);
-
+      const countResult = await UserModel.matchUser(idUser);
       // L'utilisateur a bien été trouvé dans la base de données
       if (countResult[0].count > 0) {
+        console.log("utilisateur trouvé");
         // On renvoie les informations de l'utilisateur
-        const queryResult = await UserModel.getOne(idUser, req.method);
+        const queryResult = await UserModel.read("where id=", idUser);
         res.send(queryResult);
       } else {
         res.status(406).send({ "No result for user :": idUser });
@@ -64,15 +60,14 @@ class UserController {
       let idUser = req.params.id;
 
       // On vérifie si l'utilisateur existe en base de données
-      const countResult = await UserModel.matchDB(idUser);
+      const countResult = await UserModel.matchUser(idUser);
 
       // L'utilisateur a bien été trouvé dans la base de données
       if (countResult[0].count > 0) {
-        console.log("user trouvé")
+        console.log("user trouvé");
         // On renvoie les informations de l'utilisateur
 
-
-        const queryResult = await UserModel.deleteOne(idUser);
+        const queryResult = await UserModel.delete(idUser);
 
         res.send("User deleted");
       } else {
@@ -86,7 +81,8 @@ class UserController {
   // display all users
   static async getUsers(req, res) {
     try {
-      const queryResult = await UserModel.getAll();
+      const queryResult = await UserModel.read();
+
       res.send(queryResult);
     } catch (err) {
       res.json({ message: err });
@@ -101,9 +97,9 @@ class UserController {
       const pass = "faqmdp";
       const ipAdress =
         req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-      const fields = [[name, mail, role, pass, ipAdress]];
+      const fields_table = [[name, mail, pass, ipAdress, role]];
 
-      const queryResult = await UserModel.addOne(fields);
+      const queryResult = await UserModel.create(fields_table);
       res.send("User successfully added");
     } catch (err) {
       res.json({ message: err });
@@ -119,15 +115,17 @@ class UserController {
         res.status(400).send("JSON incorrect. Champs attendus : login et mdp");
       } else {
         const queryResult = await UserModel.checkLogin(login, password);
-
+console.log("queryResult : ",queryResult)
         if (queryResult[0].count !== 0) {
           res.status(200).json("Identifiants ok");
         } else {
+         
           res.status(401).json("Identifiants incorrects");
         }
       }
     } catch (err) {
-      res.status(406).json("Identifiants incorrects");
+     
+      res.status(404);
     }
   }
 }
--- api/datasource/mysql.js
+++ api/datasource/mysql.js
@@ -1,73 +1,11 @@
-
 const mysql = require("mysql");
-
-
-class DB {
-  init() {
-    this.connection = mysql.createPool({
+require('dotenv').config();
+   module.exports = mysql.createPool({
       user: process.env.MYSQL_USER,
       password: process.env.MYSQL_PASSWORD,
       host: process.env.MYSQL_HOST,
       database: process.env.MYSQL_DATABASE,
     });
-    return this;
-  }
-
-  async query(query, ...params) {
-    return new Promise((resolve, reject) => {
-      this.connection.query(query, params, (err, result) => {
-        if (err) {
-          console.log("reject")
-          reject(err);
-        } else {
-          console.log("result")
-          resolve(result);
-        }
-      });
-    });
-  }
-
-  async queryt(...args) {
-    return new Promise((resolve, reject) => {
-      this.connection.query(...args, (err, res) => {
-        if (err) reject(err);
-        else resolve(res);
-      });
-    });
-  }
-
-  async read(table, fields, WHERE_CLAUSE, id) {
-
-    if (WHERE_CLAUSE !== undefined || id !== undefined ) {
-     
-    const query = `SELECT ${fields} FROM ${table} ${WHERE_CLAUSE} ${id}`;
-    console.log("query : ",query)
-    return this.query(query);
-   
-  } else {
-
-    const query = `SELECT ${fields} FROM ${table} `
-    return this.query(query);
-  }
-
-
   
-  }
 
-  async delete(table, id) {
-    const query = `DELETE  FROM ${table} WHERE id=${id} `;
-    return this.query(query);
-  }
 
-  async create(table, fields_table, fields) {
-    const query = ` INSERT INTO  ${table} ${fields_table} VALUES ?  `;
-    return this.query(query, fields);
-  }
-
-  async update(table, id, fields) {
-    const query = ` UPDATE ${table} SET ${fields} WHERE id= ${id} `;
-
-    return this.query(query);
-  }
-}
-module.exports = new DB().init();
