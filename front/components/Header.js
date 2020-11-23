10bf86b7a73cadac78522dee86b225d14679a8b7
IsmaelBernard <ismael.bernard69@gmail.com>
2020-11-23 15:58:41 +0100

    Added layout pages template & structure  for scale

--- front/components/Header.js
+++ front/components/Header.js
@@ -1,19 +1,39 @@
+import {useState} from "react";
 import Link from "next/link";
 import styles from "../styles/Header.module.css";
 import useModal from "../utils/useModal";
 import Modal from "./ButtonAsk";
+import * as FaIcons from "react-icons/fa";
 
 export default function Header() {
+  
+  const [sidebar, setSidebar] = useState(false);
   const { isShowing, toggle } = useModal();
+  const showSidebar = () => setSidebar(!sidebar);
+  
   return (
     <header className={styles.header}>
-      <span className={styles.headerWrapper}>
+      <div className={styles.headerWrapper}>
         <h1>
           <Link href="/">Les Freelances Lyonnais</Link>
         </h1>
         <button onClick={toggle}>Poser une question</button>
         <Modal isShowing={isShowing} hide={toggle} />
-      </span>
+      </div>
+      <div className={styles.navbar}>
+        <Link as="" href="" className={styles.menubar}> 
+          <FaIcons.FaBars onClick={showSidebar} />
+        </Link>
+      </div>
+      <nav className={ sidebar ? "navmenu active" : "navmenu"  }>
+        <ul className={styles.navmenuitems}>
+            <li className={styles.navbar-toggle}>
+              <Link as="" href="" className={styles.menubar}>
+                <FaIcons.FaRegTimesCircle/>
+              </Link>
+            </li>
+        </ul>
+      </nav>
     </header>
   );
 }
---
+++ front/components/HeaderData.js
@@ -0,0 +1,29 @@
+import * as FaIcons from "react-icons/fa";
+
+export const HeaderData = [
+
+    {
+        title:"Home",
+        path:"/",
+        icon: <FaIcons.FaCampground/>,
+        cName : "navtext"
+    },
+    {
+        title:"About-us",
+        path:"/about-us",
+        icon: <FaIcons.FaFeather/>,
+        cName : "navtext"
+    },
+    {
+        title:"Mentions legales",
+        path:"/mentions",
+        icon: <FaIcons.FaBalanceScale/>,
+        cName : "navtext"
+    },
+    {
+        title:"Sandbox",
+        path:"/test",
+        icon: <FaIcons.FaBug/>,
+        cName : "navtext"
+    },
+]
