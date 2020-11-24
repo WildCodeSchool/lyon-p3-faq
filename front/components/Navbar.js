bef78700b872f2cc355e1e7d511604ff767dd907
IsmaelBernard <ismael.bernard69@gmail.com>
2020-11-24 22:24:03 +0100

    WIP on sidebar

--- front/components/Navbar.js
+++ front/components/Navbar.js
@@ -1,8 +1,9 @@
 import {useState} from "react";
 import Link from "next/link";
-import styles from "../styles/Header.module.css";
+import styles from "../styles/Navbar.module.css"
 import * as FaIcons from "react-icons/fa";
 import {HeaderData} from "../components/HeaderData";
+
 
 export default function Header() {
   
@@ -11,21 +12,16 @@ export default function Header() {
   
   return (
     <header className={styles.header}>
-      <div className={styles.headerWrapper}>
-        <h1>
-          <Link href="/">Les Freelances Lyonnais</Link>
-        </h1>
-      </div>
+     
       <div className={styles.navbar}>
         <Link as="" href="" className={styles.menubar}> 
           <FaIcons.FaBars onClick={showSidebar} />
         </Link>
-    
       </div>
-      <nav className={'navmenu '+ (sidebar ? 'active':'') }>
-        <ul className={styles.navmenuitems} onClick={showSidebar}>
+      <nav className={sidebar ? styles["navmenu active"]: styles["navmenu"] }>
+        <ul className={styles["navmenuitems"]} onClick={showSidebar}>
             <li className={styles['navbar-toogle']}>
-              <Link as="" href="" className={styles.menubar}>
+              <Link as="" href="" className={styles["menubar"]}>
                 <FaIcons.FaRegTimesCircle/>
               </Link>
             </li>
