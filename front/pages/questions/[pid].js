b08fe65283c427da41de04003bcf1abdd1f37029
IsmaelBernard <ismael.bernard69@gmail.com>
2020-11-30 14:58:56 +0100

    passed props into pages

--- front/pages/questions/[pid].js
+++ front/pages/questions/[pid].js
@@ -17,7 +17,7 @@ export default function Question({ data }) {
           content="width=device-width, initial-scale=1.0"
         ></meta>
       </Head>
-      <Header />
+      <Header displayButton={false} errorHandler={data} />
       <Answer questionAnswered={data} />
     </>
   );
