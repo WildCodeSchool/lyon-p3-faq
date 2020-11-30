9872e0e1ab516d80f92d43aef4ca3c78deaf7d71
IsmaelBernard <ismael.bernard69@gmail.com>
2020-11-30 11:43:05 +0100

    Navbar component done

--- front/components/Navbar.js
+++ front/components/Navbar.js
@@ -3,17 +3,34 @@ import Link from "next/link";
 import styles from "../styles/Navbar.module.css"
 import * as FaIcons from "react-icons/fa";
 import {HeaderData} from "../components/HeaderData";
+import useModal from "../utils/useModal";
+import Modal from "./ButtonAsk";
 
 
 export default function Navbar() {
   
   const [sidebar, setSidebar] = useState(false);
   const showSidebar = () => setSidebar(!sidebar);
+  const { isShowing, toggle } = useModal();
   
   return (
     <header>
       <div className={styles.navbar}>
-          <FaIcons.FaBars onClick={showSidebar} />
+        
+        <div className={styles.navcontainer}>
+          <div className={styles.leftwrapnav}>
+            <h1>
+              <Link href="/">Les Freelances Lyonnais</Link>
+            </h1>
+          </div>
+
+          <div className={styles.rightwrapnav}>
+            <button  className={styles.navbutton} onClick={toggle}>Poser une question</button>
+            <Modal isShowing={isShowing} hide={toggle} />
+            <FaIcons.FaBars onClick={showSidebar} />
+          </div>
+        </div>
+
       </div>
 
       <nav className={sidebar ? styles["navmenuactive"] : styles["navmenu"] }>
