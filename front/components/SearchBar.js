9e07b37f2ba7882172649833ac8cba47878a8a20
IsmaelBernard <ismael.bernard69@gmail.com>
2020-12-11 10:09:47 +0100

    WIP

--- front/components/SearchBar.js
+++ front/components/SearchBar.js
@@ -1,20 +1,32 @@
 import * as FaIcons from "react-icons/fa";
 import styles from "../styles/SearchBar.module.css";
+import {useState, useEffect} from "react";
 
 
 export default function SearchBar() {
+
+    const [search, setSearch] = useState("")
+
+
     return (
         <div className={styles.searchbar}>
             <div className={styles.innersearchleft}>
             <FaIcons.FaSearch/>
             </div>
             <div className={styles.innersearchmid}>  
-            <input type="text" placeholder="recherche" className={styles.searchinput}
+            <input 
+            value = {search}
+            onChange = {event => setSearch(event.target.value)}
+            type="text" 
+            placeholder="recherche" 
+            className={styles.searchinput}
             />
             </div>
             <div className={styles.innersearchright}>
               <FaIcons.FaTimes/>
             </div>
         </div>
+
     );
 };
+
