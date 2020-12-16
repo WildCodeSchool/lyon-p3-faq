005f466ec4ed12fc4eb0fc1739f1583addf29fea
Maxence <maxence.meloni@wildcodeschool.com>
2020-12-16 19:16:09 +0100

    [API] fix mysql connector

--- api/datasource/mysql.js
+++ api/datasource/mysql.js
@@ -3,11 +3,15 @@ const dotenv = require("dotenv").config();
 class Database {
   init() {
     this.connection = mysql.createPool({
-
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      host: process.env.MYSQL_HOST,
+      database: process.env.MYSQL_DATABASE,
+      localAddress: process.env.MYSQL_LOCALADDRESS
     })
     return this;
   }
-  
+
   async query (...args) {
     return new Promise((resolve, reject) => {
       this.connection.query(...args, (err, res) => {
