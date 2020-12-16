ead55d8f14f3485666582f3bc29750b6bd7c599c
IsmaelBernard <ismael.bernard69@gmail.com>
2020-12-16 16:45:09 +0100

    Added backup

--- api/datasource/mysql.js
+++ api/datasource/mysql.js
@@ -1,5 +1,8 @@
 const mysql = require("mysql");
 require("dotenv").config();
 module.exports = mysql.createPool({
-
+    user: "root",
+  password: "",
+  host: "localhost",
+  database: "faqosdbpres",
 });
